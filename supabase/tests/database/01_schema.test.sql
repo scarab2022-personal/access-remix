02e83c5b42ef88f49ab02609534b021ffedff6f5
Michael Wu <mw10013@gmail.com>
2022-12-10 20:38:55 -0500

    pgtap

--- supabase/tests/database/01_schema.test.sql
+++ supabase/tests/database/01_schema.test.sql
@@ -1,6 +1,6 @@
 begin;
 
-select plan (10);
+select plan (8);
 
 -- tables
 select has_table ('access_hub', 'access_hub should exist');
@@ -12,15 +12,11 @@ select has_table ('access_user', 'access_user should exist');
 select has_table ('access_point_to_access_user', 'access_point_to_access_user should exist');
 
 -- triggers
-select has_trigger ('auth', 'users', 'on_new_auth_user_before', 'trigger on_new_auth_user_before should exist');
+select trigger_is ('auth', 'users', 'on_new_auth_user_before', 'public', 'on_new_auth_user_before', 'trigger on_new_auth_user_before should exist');
 
-select has_trigger ('auth', 'users', 'on_new_auth_user_after', 'trigger on_new_auth_user_after should exist');
+select trigger_is ('auth', 'users', 'on_new_auth_user_after', 'public', 'on_new_auth_user_after', 'trigger on_new_auth_user_after should exist');
 
 -- functions
-select has_function ('on_new_auth_user_before', array[]::name[], 'on_new_auth_user_before should exist');
-
-select has_function ('on_new_auth_user_after', array[]::name[], 'on_new_auth_user_after exist');
-
 select has_function ('connect_access_points_and_users', '{integer[], integer[], uuid}'::name[], 'connect_access_points_and_users should exist');
 
 select has_function ('disconnect_access_points_and_users', '{integer[], integer[], uuid}'::name[], 'connect_access_points_and_users should exist');
