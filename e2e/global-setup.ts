bfedf3c18d6bffe66c9aff99be119dc3e5266cdc
Michael Wu <mw10013@gmail.com>
2022-12-18 17:15:27 -0500

    test: use dreadfulcompany.com for customer

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -1,6 +1,6 @@
 // global-setup.ts
 import type { Browser, FullConfig, Page } from "@playwright/test";
-import { chromium, expect } from "@playwright/test";
+import { chromium } from "@playwright/test";
 
 async function saveStorageState(
   email: string,
@@ -32,6 +32,7 @@ async function saveStorageState(
 
 async function globalSetup(config: FullConfig) {
   const browser = await chromium.launch();
+
   await saveStorageState(
     "scarab2022@gmail.com",
     "http://localhost:54324/m/scarab2022",
@@ -39,40 +40,12 @@ async function globalSetup(config: FullConfig) {
     browser
   );
   await saveStorageState(
-    "customer@access.com",
+    "customer@dreadfulcompany.com",
     "http://localhost:54324/m/customer",
     "e2e-results/storage-states/customerStorageState.json",
     browser
   );
 
-  // const page = await browser.newPage();
-
-  // // Delete any existing emails.
-  // await page.goto("http://localhost:54324/m/scarab2022");
-  // await page.getByRole("button", { name: "" }).last().click();
-  // await page.getByText("delete all messages");
-  // await page.getByRole("button", { name: "yes" }).click();
-  // await expect(page.getByRole("complementary").nth(1)).toBeEmpty();
-
-  // await page.goto("http://localhost:3000");
-  // await expect(page).toHaveTitle(/Access Remix/);
-  // await page.getByRole("link", { name: "sign in" }).click();
-  // await page.getByLabel("Email").fill("scarab2022@gmail.com");
-  // await page.getByRole("button", { name: "sign in" }).click();
-  // await expect(page.getByText("check your email")).toBeVisible();
-
-  // await page.goto("http://localhost:54324/m/scarab2022");
-  // await page.getByText("Your Magic Link").first().click();
-  // await page.getByRole("link", { name: "Log In" }).click();
-  // await expect(page).toHaveTitle(/Access Remix/);
-  // await page.getByRole("link", { name: "Enter" }).click();
-  // await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible();
-
-  // await page
-  //   .context()
-  //   .storageState({
-  //     path: "e2e-results/storage-states/adminStorageState.json",
-  //   });
   await browser.close();
 }
 
