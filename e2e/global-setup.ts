170f029b766a048ae043c327d0ae5aecd93716b7
Michael Wu <mw10013@gmail.com>
2022-12-18 13:17:45 -0500

    test: add global storage state

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -1,7 +1,11 @@
-import { test, expect } from "@playwright/test";
+// global-setup.ts
+import type { FullConfig } from "@playwright/test";
+import { chromium, expect } from "@playwright/test";
 
-/*
-test("sign in", async ({ page, context }) => {
+async function globalSetup(config: FullConfig) {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
   // Delete any existing emails.
   await page.goto("http://localhost:54324/m/scarab2022");
   await page.getByRole("button", { name: "" }).last().click();
@@ -9,7 +13,7 @@ test("sign in", async ({ page, context }) => {
   await page.getByRole("button", { name: "yes" }).click();
   await expect(page.getByRole("complementary").nth(1)).toBeEmpty();
 
-  await page.goto("/");
+  await page.goto("http://localhost:3000");
   await expect(page).toHaveTitle(/Access Remix/);
   await page.getByRole("link", { name: "sign in" }).click();
   await page.getByLabel("Email").fill("scarab2022@gmail.com");
@@ -23,13 +27,8 @@ test("sign in", async ({ page, context }) => {
   await page.getByRole("link", { name: "Enter" }).click();
   await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible();
 
-  await context.storageState({ path: "scarab2022-state.json" });
-});
-*/
+  await page.context().storageState({ path: "storageState.json" });
+  await browser.close();
+}
 
-test("enter", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle(/Access Remix/);
-  await page.getByRole("link", { name: "Enter" }).click();
-  await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible();
-});
+export default globalSetup;
--- playwright.config.ts
+++ playwright.config.ts
@@ -31,15 +31,18 @@ const config: PlaywrightTestConfig = {
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
+
+  globalSetup: require.resolve("./e2e/global-setup"),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    // headless: false,
     // browserName: 'chromium',
 
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:3000",
+    storageState: 'storageState.json',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
