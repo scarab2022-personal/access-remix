45f3bbe70af93b23bfc9eb9b346ad2130b500d0e
Michael Wu <mw10013@gmail.com>
2022-12-19 14:24:11 -0500

    test: add customer navigation test

--- e2e/customer.spec.ts
+++ e2e/customer.spec.ts
@@ -6,3 +6,25 @@ test("enter", async ({ page }) => {
   await page.getByRole("link", { name: "Enter" }).click();
   await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible();
 });
+
+test("navigation", async ({ page }) => {
+  await page.goto("/access/dashboard");
+  await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible();
+  await page.getByRole("link", { name: "Users" }).click();
+  await expect(page.getByRole("heading", { name: "Users" })).toBeVisible();
+  await page.getByRole("link", { name: "Hubs" }).click();
+  await expect(page.getByRole("heading", { name: "Hubs" })).toBeVisible();
+  await page.getByRole("link", { name: "Hub 1" }).click();
+  await expect(page.getByRole("heading", { name: "Hub 1" })).toBeVisible();
+  await page.getByRole("link", { name: "View" }).first().click();
+  await expect(page.getByRole("heading", { name: "Point 1" })).toBeVisible();
+  await page.getByRole("button", { name: "Edit" }).click();
+  await expect(
+    page.getByRole("heading", { name: "Access Point Settings" })
+  ).toBeVisible();
+  await page
+    .getByRole("navigation", { name: "Breadcrumb" })
+    .getByRole("link", { name: "Hub",exact: true })
+    .click();
+  await expect(page.getByRole("heading", { name: "Hub 1" })).toBeVisible();
+});
