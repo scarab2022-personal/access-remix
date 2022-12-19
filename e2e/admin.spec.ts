aa2f85a58015dcd9ff14c04df6e018908a262244
Michael Wu <mw10013@gmail.com>
2022-12-19 13:25:10 -0500

    test: add admin navigation test

--- e2e/admin.spec.ts
+++ e2e/admin.spec.ts
@@ -8,3 +8,31 @@ test("enter", async ({ page }) => {
   await page.getByRole("link", { name: "Enter" }).click();
   await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible();
 });
+
+test("navigation", async ({ page }) => {
+  await page.goto("/admin/dashboard");
+  await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible();
+  await page.getByRole("link", { name: "Customers" }).click();
+  await expect(page.getByRole("heading", { name: "Customers" })).toBeVisible();
+  await page.getByRole("link", { name: "View" }).first().click();
+  await expect(
+    page.getByRole("heading", { name: "Access Hubs" })
+  ).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Access Users" })
+  ).toBeVisible();
+  await page
+    .getByRole("navigation", { name: "Breadcrumb" })
+    .getByRole("link", { name: "Customers" })
+    .click();
+  await expect(page.getByRole("heading", { name: "Customers" })).toBeVisible();
+  await page.getByRole("link", { name: "View" }).last().click();
+  await expect(
+    page.getByRole("heading", { name: "Access Hubs" })
+  ).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Access Users" })
+  ).toBeVisible();
+  await page.getByRole("link", { name: "Dashboard" }).click();
+  await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible();
+});
